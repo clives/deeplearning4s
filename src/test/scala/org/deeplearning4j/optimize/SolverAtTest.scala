d58b6c055125c4f9fe7768911e7757017b463683
Shingo Omura <everpeace@gmail.com>
2015-08-14 12:04:12 +0900

    fix SolverAt, changed to accept DL4S's Layer instance as its input.

--- src/test/scala/org/deeplearning4j/optimize/SolverAtTest.scala
+++ src/test/scala/org/deeplearning4j/optimize/SolverAtTest.scala
@@ -1,18 +1,17 @@
 package org.deeplearning4j.optimize
 
-import org.deeplearning4j.nn.api.Layer
-import org.deeplearning4j.nn.conf.layers.RBM
-import org.deeplearning4j.nn.layers.factory.LayerFactories
+import org.deeplearning4j.nn.conf.layers.{RBM => jRBM}
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener
 import org.deeplearning4s.nn.conf.NeuralNetConf
+import org.deeplearning4s.nn.conf.layers.RBM
+import org.deeplearning4s.nn.conf.layers.factory.CreateLayerAt
 import org.deeplearning4s.optimize.SolverAt
 import org.scalatest.FlatSpec
 
 class SolverAtTest extends FlatSpec {
   "SolverAt" should "create a solver to return optimizer without exception" in {
-    // FIX these sample creation after developing scala bindings for nn.conf.layers and nn.api.layers.
-    val conf = NeuralNetConf(new RBM.Builder(RBM.HiddenUnit.BINARY, RBM.VisibleUnit.BINARY, 1).build()).asJava
-    val model: Layer = LayerFactories.getFactory(conf.getLayer()).create(conf);
+    val conf = NeuralNetConf(RBM(jRBM.HiddenUnit.BINARY, jRBM.VisibleUnit.BINARY, 1))
+    val model = CreateLayerAt(conf)
     val listeners = Seq(new ScoreIterationListener())
 
     SolverAt(conf, model, listeners).getOptimizer
